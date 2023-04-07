bfed32b44db4abfa1b3a6714b7f03e3d79507ade
Jason Fu <gimpact709@gmail.com>
2023-04-07 22:43:45 +0200

    routing for grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TutorialComponent } from './Components/tutorial/tutorial.component';
+import { GridComponent } from './Components/grid/grid.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/tutorial', pathMatch: 'full' },
   { path: "tutorial", component: TutorialComponent },
+  { path: "pathfinder", component: GridComponent},
 ];
 
 @NgModule({
